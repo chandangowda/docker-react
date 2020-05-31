9dd417035458bbdafa8cd15269a9784fd3d26565
Chandangowda <chandangowda.chikkabasavanahallibaburam@centurylink.com>
2020-05-31 21:34:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           nskldf Thn
         </p>
         <a
           className="App-link"
